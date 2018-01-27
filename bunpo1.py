f9db3d5c56640c889934270b9d26d3426cb790f9
hideo <sankamonohideo@sankamoeiseinoMacBook-Pro.local>
2018-01-27 13:46:13 +0900

    copy test

--- bunpo1.py
+++ bunpo1.py
@@ -2,3 +2,17 @@ import numpy as np
 a = np.array = ([1,2,3,4,5])
 b = np.array = ([2,3,4,5,6])
 
+c = b
+c[0]=11
+# change for c affected b
+d = b.copy()
+d[0]=1
+# change for d not affected b
+
+print (b)
+print (np.dot(a,b))
+print (c)
+print (np.dot(a,d))
+
+print ("sa =",np.dot(a,b)-np.dot(a,d))
+
