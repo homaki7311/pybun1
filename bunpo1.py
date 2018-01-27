a89e7f09357b14b92bcc880a00a95522865a4e1c
hideo <sankamonohideo@sankamoeiseinoMacBook-Pro.local>
2018-01-27 14:16:35 +0900

    change details

--- bunpo1.py
+++ bunpo1.py
@@ -1,9 +1,9 @@
 import numpy as np
-a = np.array = ([1,2,3,4,5])
-b = np.array = ([2,3,4,5,6])
+a = np.array  ([1,2,3,4,5])
+b = np.array  ([2,3,4,5,6])
 
 c = b
-c[0]=11
+c[0]=3
 # change for c affected b
 d = b.copy()
 d[0]=1
@@ -11,7 +11,7 @@ d[0]=1
 
 print (b)
 print (np.dot(a,b))
-print (c)
+print (d)
 print (np.dot(a,d))
 
 print ("sa =",np.dot(a,b)-np.dot(a,d))
