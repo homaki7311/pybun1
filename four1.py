d07ece2acfd020dff6f17fb56a6c3cd31b47ec1b
hideo <sankamonohideo@sankamoeiseinoMacBook-Pro.local>
2018-02-18 10:07:52 +0900

    fourier

---
+++ four1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import quad
+
+def f(x):
+    return x*x*x-0.8*x*x
+
+pi = np.pi
+T = 2*pi
+jisuu = 15
+omega = T / (2*pi)
+a = []
+b = []
+for num in range(0,jisuu):
+    a.append(quad(lambda x,k : f(x)*np.cos(k*omega*x),-T/2,T/2,args = num)[0])
+    b.append(quad(lambda x,k : f(x)*np.sin(k*omega*x),-T/2,T/2,args = num)[0])
+
+def f2(x):
+    result = a[0]/2*2/T
+    for num in range(1,jisuu):
+        result = result + 2/T*a[num]*np.cos(num*omega*x)+2/T*b[num]*np.sin(num*omega*x)
+    return result
+
+xx = np.linspace(-T/2,T/2,200)
+yy = f2(xx)
+
+plt.plot(xx,yy)
+plt.plot(xx,f(xx))
+plt.show()
