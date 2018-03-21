11cc6e5efe259b2c9413530237a026b1430439a4
hideo <sankamonohideo@sankamoeiseinoMacBook-Pro.local>
2018-03-21 10:23:55 +0900

    collections

--- fibun1.py
+++ fibun1.py
@@ -1,7 +1,13 @@
 import csv
+import collections
 CsvFile = csv.reader(open('biblio.csv'),delimiter=',')
 CsvList = []
 for num in CsvFile:
     CsvList.append(num)
-for num in range(0,len(CsvList)):
-    print(num,CsvList[num][5][0:15].strip())
+total = len(CsvList)
+FI = []
+for num in range(0,total):
+    FI.append(CsvList[num][5][0:15].strip())
+count_dict = collections.Counter(FI)
+
+print(count_dict)
